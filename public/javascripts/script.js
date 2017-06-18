38f88bbc34c2c6935b3f19dd8bf775b64c0e8ab6
Sandeep Vattapparambil <sandeepv68@gmail.com>
2017-06-18 12:49:42 +0530

    nprogress updates

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,27 @@
 /*
 Sandeep Vattapparambil
 */
+//Vanilla javascript
+function detectmob() {
+  if (navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    console.log('Mobile detected');
+    return true;
+  } else {
+    console.log('Desktop detected');
+    return false;
+  }
+}
+NProgress.start();
+//jquery
 $(document).ready(function() {
+  detectmob();
+  NProgress.done();
   $(".button-collapse").sideNav();
 });
