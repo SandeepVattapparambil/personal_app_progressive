e88799e8e809798bf1ee744fc6fe8ca92905a0f1
Sandeep Vattapparambil <sandeepv68@gmail.com>
2017-06-18 12:04:23 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sandeep Vattapparambil' });
+  res.render('index', {
+    title: 'Sandeep Vattapparambil',
+    logo: 'Sandeep V'
+  });
 });
 
 module.exports = router;
